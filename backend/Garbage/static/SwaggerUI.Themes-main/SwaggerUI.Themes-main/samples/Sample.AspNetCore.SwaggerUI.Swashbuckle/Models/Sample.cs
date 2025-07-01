a801a30402caa515a87a5548d8366c1195a7b4af
Ethan Orain <67012865+Erazor48@users.noreply.github.com>
2025-07-02 00:46:31 +0200

    Delete backend/Garbage directory
    Delete Garbage.

--- backend/Garbage/static/SwaggerUI.Themes-main/SwaggerUI.Themes-main/samples/Sample.AspNetCore.SwaggerUI.Swashbuckle/Models/Sample.cs
+++
@@ -1,8 +0,0 @@
-﻿#pragma warning disable S1133 // Deprecated code should be removed - Done for testing purposes
-
-using System.ComponentModel.DataAnnotations;
-
-namespace Models;
-
-[Obsolete("test")]
-public record Sample(WeatherForecast WeatherForecast, [Required] double Value);
--- backend/Garbage/static/SwaggerUI.Themes-main/SwaggerUI.Themes-main/src/AspNetCore.Swagger.Themes.Common/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AspNetCore.SwaggerUI.Themes")]
-[assembly: InternalsVisibleTo("NSwag.AspNetCore.Themes")]
-[assembly: InternalsVisibleTo("AspNetCore.Swagger.Themes.Tests")]
